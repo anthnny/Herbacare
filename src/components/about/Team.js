048069b8fbb64c24a8774756ae3e189a7462330a
anthnny <zanthonygg@gmail.com>
2023-08-03 16:41:01 -0500

    build

--- src/components/about/Team.js
+++ src/components/about/Team.js
@@ -3,9 +3,28 @@ import anthony from 'assets/img/anthony.jpeg'
 const people = [
     {
       name: 'Anthony Lopez',
-      role: 'Back-end Developer',
+      role: 'Desarrollador Back-end',
       twitterUrl: 'https://twitter.com/AnthnnyL',
-      linkedinUrl: '#',
+    },
+    {
+      name: 'Anderson Merchan',
+      role: 'Administrador del Proyecto',
+      twitterUrl: '#',
+    },
+    {
+      name: 'Dary Pincay',
+      role: 'Front-end Developer',
+      twitterUrl: '#',
+    },
+    {
+      name: 'Michel Romero',
+      role: 'Front-end Developer',
+      twitterUrl: '#',
+    },
+    {
+      name: 'Ruben Bones',
+      role: 'Front-end Developer',
+      twitterUrl: '#',
     },
   ]
   
@@ -46,15 +65,30 @@ const people = [
                             </svg>
                           </a>
                         </li>
+                      </ul>
+                    </div>
+                  </div>
+                </li>
+              ))}
+
+              {people.map((person) => (
+                <li key={person.name}>
+                  <div className="space-y-4">
+                    <div className="aspect-w-3 aspect-h-3">
+                      <img className="rounded-lg object-cover shadow-lg" src={'https://bafybeif7dn2vmg27qkit5wo35qnitqftvkbrlqldv5xy6mol7onwk52g34.ipfs.w3s.link/anderson.jpeg'} alt=""/>
+                    </div>
+  
+                    <div className="space-y-2">
+                      <div className="space-y-1 text-lg font-medium leading-6">
+                        <h3>{person.name}</h3>
+                        <p className="text-indigo-600">{person.role}</p>
+                      </div>
+                      <ul role="list" className="flex space-x-5">
                         <li>
-                          <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
-                            <span className="sr-only">LinkedIn</span>
+                          <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500"  target="_blank" >
+                            <span className="sr-only">Twitter</span>
                             <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                              <path
-                                fillRule="evenodd"
-                                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                                clipRule="evenodd"
-                              />
+                              <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
                             </svg>
                           </a>
                         </li>
--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,48 +1,47 @@
 import { Typewriter } from "react-simple-typewriter"
 
-function Header(){
-    return(
+function Header() {
+    return (
         <main>
-        <div className="relative ">
-          <div className="mx-auto xl:mx-12 xl:pt-48 xl:pb-64 lg:pt-40 lg:pb-48 pt-24 pb-12  ">
-            <div>
-              <div>
-                <h1 className="text-4xl font-semibold tracking-tight pb-16 sm:text-7xl">
-                  Remedios <span> </span>
-                    <div className="inline-flex" style={{ color: '#b5ca92', fontWeight: 'bold' }}>
-                      <Typewriter
-                            words={['Caseros', 'Herbales']}
-                            loop={0}
-                            cursor
-                            
-                            cursorStyle='_'
-                            typeSpeed={120}
-                            deleteSpeed={50}
-                            delaySpeed={1000}
-                        />
+            <div className="relative ">
+                <div className="mx-auto xl:mx-12 xl:pt-48 xl:pb-64 lg:pt-40 lg:pb-48 pt-24 pb-12  ">
+                    <div className="flex flex-col-reverse lg:flex-row-reverse">
+                        <div className="lg:w-2/3">
+                            <h1 className="text-4xl font-semibold tracking-tight pb-16 sm:text-7xl">
+                                Remedios <span> </span>
+                                <div className="inline-flex" style={{ color: '#b5ca92', fontWeight: 'bold' }}>
+                                    <Typewriter
+                                        words={['Caseros', 'Herbales']}
+                                        loop={0}
+                                        cursor
+                                        cursorStyle='_'
+                                        typeSpeed={120}
+                                        deleteSpeed={50}
+                                        delaySpeed={1000}
+                                    />
+                                </div>
+                            </h1>
+                            <p className="mt-16 text-2xl max-w-2xl leading-8 text-black">
+                                La naturaleza nos brinda un tesoro de remedios, pero la seguridad y el bienestar son la prioridad número uno.
+                                ¡Explora el mundo de las plantas medicinales con prudencia y disfruta de sus increíbles beneficios!
+                            </p>
+                            <div className="mt-8 flex gap-x-4">
+                                <a
+                                    href="/remedios"
+                                    className="inline-block rounded-lg bg-green-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-white-600 hover:bg-green-700 hover:ring-green-700"
+                                >
+                                    Ver Remedios
+                                </a>
+                            </div>
+                        </div>
+                        <div className="lg:w-1/3 mt-10 lg:mt-0">
+                            <img src="https://bafybeidlzoertkt2dnsvg75pafwxzll2uughbiuklwi5eeyexbdq7fc7rq.ipfs.w3s.link/herba.png" className='w-full h-auto object-cover'/>
+                        </div>
                     </div>
-                </h1>
-                <p className="mt-16 text-2xl max-w-2xl leading-8 text-black">
-                La naturaleza nos brinda un tesoro de remedios, pero la seguridad y el bienestar son la prioridad número uno. 
-                ¡Explora el mundo de las plantas medicinales con prudencia y disfruta de sus increíbles beneficios!
-                </p>
-                <div className="mt-8 flex gap-x-4">
-                  <a
-                    href="/remedios"
-                    className="inline-block rounded-lg bg-green-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-white-600 hover:bg-green-700 hover:ring-green-700"
-                  >
-                    Ver Remedios
-                  </a>
                 </div>
-              </div>
-              <div className="absolute top-[calc(100%-20rem)] -z-10 transform-gpu overflow-hidden bg-white backdrop-blur-lg lg:top-[calc(100%-45rem)] sm:top-[calc(100%-30rem)] right-20 w-1/3">
-                <img src="https://bafybeidlzoertkt2dnsvg75pafwxzll2uughbiuklwi5eeyexbdq7fc7rq.ipfs.w3s.link/herba.png" className='w-full h-auto object-cover'/>
-              </div>
             </div>
-          </div>
-        </div>
-      </main>
+        </main>
     )
 }
 
-export default Header
+export default Header;
